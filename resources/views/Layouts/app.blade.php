571feba9b6a688eab57298b2936163ab2ea76473
Rizki Si <rizkisi@email.com>
2025-05-19 12:28:50 +0700

    tailwind commit

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -2,7 +2,8 @@
 
 <html>
 
-<head>
+<head> 
+    @vite('resources/css/app.css')
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
